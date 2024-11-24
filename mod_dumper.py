dfed2fcb29447049e2e5230525e8905a983ca302
XenoHoe <88544456+XenoHoe@users.noreply.github.com>
2024-11-24 14:55:38 +0800

    Mod list sort by alphabet
    Self-explanatory

--- mod_dumper.py
+++ mod_dumper.py
@@ -16,12 +16,13 @@ def dump_files_in_folder(folder_path, output_file="file_dump.txt"):
     try:
         with open(output_file, 'w') as file_output:
             for root, dirs, files in os.walk(folder_path):
-                for filename in files:
+                for filename in sorted(files):
                     full_path = filename #Only the file name is neccesary
                     file_output.write(full_path + '\n')
         print(f"File list successfully saved to '{output_file}'")
     except Exception as e:
         print(f"An error occurred: {e}")
+
 
 # Call the function
 dump_files_in_folder(TARGET_FOLDER,FILE_NAME)
