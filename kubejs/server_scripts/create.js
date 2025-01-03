9dc25e9f97e0750dcb145ed51a046202659178a9
XenoHoe <88544456+XenoHoe@users.noreply.github.com>
2025-01-03 21:36:28 +0800

    More QoL recipes
    Added bucket from compacting (more like pressing in a basin shaped mold) iron sheets and quadrupled the yield of fan catalysts

--- kubejs/server_scripts/create.js
+++ kubejs/server_scripts/create.js
@@ -24,9 +24,21 @@ ServerEvents.recipes(event => {
     event.remove({'type':'create:splashing','input':'minecraft:red_sand'})
     event.recipes.create.splashing([Item.of('gold_nugget',3).withChance(.125),Item.of('redstone',5).withChance(.2)],'minecraft:red_sand',200)
 
+    //Why is it shaped?
     event.remove({'type':'minecraft:crafting_shaped','output':'minecraft:diorite'})
     event.shapeless('diorite',['cobblestone','quartz'])
 
-    
+    //Bucket from Iron sheet mold pressing
+    event.recipes.create.compacting('minecraft:bucket','create:iron_sheet')
 
+    //Cheaper create connected fan catalysts
+    event.remove({'type':'crafting_shaped','output':'create_connected:empty_fan_catalyst'})
+    event.shaped(Item.of('create_connected:empty_fan_catalyst',4),[
+        'IBI',
+        'B B',
+        'IBI'
+    ],{
+        I:'create:brass_ingot',
+        B:'minecraft:iron_bars'
+    })
 })
